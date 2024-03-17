d5d6eb9c6f903eb5c17da7e493360d1dcc64e231
sheraliyev <sheraliyev1310@gmail.com>
2024-03-17 10:11:00 +0500

    first commit

---
+++ user/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    STATUS = (
+        ('Direktor', 'Direktor'),
+        ('Admin', 'Admin'),
+    )
+    status = models.CharField(max_length=10, choices=STATUS, default='Admin')
+
+
+# Create your models here.
